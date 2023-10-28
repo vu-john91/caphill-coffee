85517f923ee24873288838ecffda83315762f8b2
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-28 12:45:04 -0600

    Set up basic CardContainer structure with imports/exports

---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import './CardContainer.css'
+import Card from '../Card/Card'
+
+const CardContainer = () => {
+  return (
+    <div className='card-container'>
+      <h2>Hello from Card Container</h2>
+      <Card/>
+    </div>
+  )
+}
+
+export default CardContainer
