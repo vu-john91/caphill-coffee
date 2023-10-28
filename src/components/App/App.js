daf12cc90cc66cb62385f3aa68f1ad4aed951fc2
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-28 12:43:55 -0600

    Set up basic App component structure with imports/exports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Error from '../Error/Error'
+import Header from '../Header/Header'
+import CardContainer from '../CardContainer/CardContainer'
+import { Routes, Route, Link, useLocation } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <h1>Hello from App</h1>
+      <Routes>
+        <Route path="/" element={<CardContainer/>}/>
+        <Route path="/*" element={<Error/>}/>
+      </Routes>
     </div>
   );
 }
