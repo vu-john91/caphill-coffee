040157a920a6a1ea45e78e97b3ea2100c8c800bf
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-28 15:40:02 -0600

    Format container

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -4,9 +4,16 @@ import Card from '../Card/Card'
 const CardContainer = () => {
   return (
     <div className='card-container'>
-      <h2>Hello from Card Container</h2>
       <Card/>
-      <h2>Hello from Card Container</h2>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </div>
   )
 }
