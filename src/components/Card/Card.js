de60b7a70102950e5176ed1843c063e775562d5d
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-10-31 19:07:13 -0600

    GET network request functionaility

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,10 @@
 import './Card.css'
 
-const Card = () => {
+const Card = ( { name, img }) => {
   return (
     <div className='card'>
-      <img src="https://ir.4sqi.net/img/general/original/3277_DPE4uVcVV57ISXxnBAtSV1n_2kgh2jFcCKt5X1ilPDA.jpg"/>
-      <h3>Hudson Hill</h3>
+      <img src={img}/>
+      <h3>{name}</h3>
       <h4>Rating: 100%</h4>
     </div>
   )
