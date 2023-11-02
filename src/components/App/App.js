fd75ade4b224c7d9a01bd12e44b583509a78aee3
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-02 17:52:46 -0600

    Attemping POST for SelectedShop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,17 +4,37 @@ import Header from "../Header/Header";
 import CardContainer from "../CardContainer/CardContainer";
 import SelectedShop from "../SelectedShop/SelectedShop";
 import sampleCoffeeShops from "../../MockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 
 function App() {
-  const [shops, setShops] = useState(sampleCoffeeShops);
+  const [shops, setShops] = useState([]);
+
+  const getShops = () => {
+    return fetch(`http://localhost:3001/`)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`)
+        }
+        console.log('hello', response.json)
+        return response.json()
+      })
+  }
+  
+  useEffect(() => {
+    getShops()
+      .then(data => setShops(data))
+      .catch(error => console.log(error.message))
+  }, [])
+  
+
+
 
   return (
     <div className='App'>
       <Header />
       <Routes>
-        <Route path='/' element={<CardContainer />} />
+        <Route path='/' element={<CardContainer shops={shops}/>} />
         <Route path='/SelectedShop/:id' element={<SelectedShop shops={shops} />} />
         <Route path='/*' element={<Error />} />
       </Routes>
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,31 +3,31 @@ import React, { useState, useEffect } from 'react'
 import Card from '../Card/Card'
 import { Routes, Route, Link, useLocation } from 'react-router-dom'
 
-const CardContainer = () => {
+const CardContainer = ( { shops }) => {
 
-const [shops, setShops] = useState([])
+// const [shops, setShops] = useState([])
 
-const getShops = () => {
-  return fetch(`http://localhost:3001/api/v1/pathData`)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`)
-      }
-      console.log('hello', response.json)
-      return response.json()
-    })
-}
+// const getShops = () => {
+//   return fetch(`http://localhost:3001/`)
+//     .then(response => {
+//       if(!response.ok) {
+//         throw new Error(`${response.status} ${response.statusText}`)
+//       }
+//       console.log('hello', response.json)
+//       return response.json()
+//     })
+// }
 
-useEffect(() => {
-  getShops()
-    .then(data => setShops(data))
-    .catch(error => console.log(error.message))
-}, [])
+// useEffect(() => {
+//   getShops()
+//     .then(data => setShops(data))
+//     .catch(error => console.log(error.message))
+// }, [])
 
   //http://localhost:3001/api/v1/pathData
 const shopCards = shops.map((shop) => {
   return (
-    <Link to={`/${shop.id}`} key={shop.id}>
+    <Link to={`/SelectedShop/${shop.id}`} key={shop.id}>
       <Card
         name={shop.name}
         img={shop.img}
